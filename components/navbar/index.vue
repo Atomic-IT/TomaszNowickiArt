8453c2072efc39acaf96fa20be806fc253e4da06
KatarzynaS97 <katarzyna33445@wp.pl>
2025-01-01 12:15:49 +0100

    Navbar component

---
+++ components/navbar/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav class="navbar">
+    <p class="navbar-heading">Tomasz Nowicki</p>
+    <ul class="navbar-items">
+      <li><a href="" class="navbar-items-link">STRONA GŁÓWNA</a></li>
+      <li><a href="" class="navbar-items-link">O MNIE</a></li>
+      <li><a href="" class="navbar-items-link">GALERIA</a></li>
+      <li><a href="" class="navbar-items-link">KONTAKT</a></li>
+    </ul>
+  </nav>
+</template>
+
+<script></script>
+
+<style lang="scss">
+@use 'index';
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="index">
+    <navbar />
     <about />
     <gallery />
     <contact />
