6e50f81de144ac087093e412ef7a5f98183f5aab
szymcode <szymon.radomski@yahoo.com>
2025-01-02 01:04:56 +0100

    Gallery improvements

--- components/home/gallery/index.vue
+++ components/home/gallery/index.vue
@@ -4,16 +4,22 @@
     <swiper
       class="gallery-swiper"
       :modules="[Autoplay]"
+      :grab-cursor="true"
       :slides-per-view="1"
       :breakpoints="{
         '600': {
           slidesPerView: 2,
+          slidesPerGroup: 2,
         },
         '1200': {
           slidesPerView: 3,
+          slidesPerGroup: 3,
         },
       }"
-      autoplay
+      :autoplay="{
+        delay: 5000,
+      }"
+      :lazy="true"
       free-mode
       loop
     >
@@ -40,18 +46,30 @@ import { Swiper, SwiperSlide } from 'swiper/vue'
 import 'swiper/swiper-bundle.css'
 
 const imageBaseNames = [
-  'castle-tree',
-  'floating-pyramid',
-  'foggy-portal',
-  'hovering-pyramid',
-  'in-the-air',
+  'glacier',
+  'circle',
+  'nocturne',
   'in-the-clouds',
-  'mystical-pyramid',
-  'nokturn',
+  'foggy-portal',
+  'in-the-air',
+  'castle-tree',
+  'spire',
+  'hovering-pyramid',
   'pyramid',
-  'surreal-tree',
-  'tower',
+  'floating-pyramid',
+  'mystical-pyramid',
+  'fire-walk-with-me',
+  'ink-flood',
+  'enormous-loneliness',
   'weeping-willow',
+  'surreal-tree',
+  'barrens',
+  'without-title-2',
+  'frozen-tree',
+  'tower',
+  'without-title-1',
+  'uroboros',
+  'dragon-hunting',
 ]
 
 const images = imageBaseNames.map((name) => ({
